0df1242332dee04ff37eca187d599d58270f1236
kostoev05 <kostoev.05@mail.ru>
2022-06-17 22:07:30 +0300

    realize user model

--- src/main/java/ru/whattime/whattime/model/User.java
+++ src/main/java/ru/whattime/whattime/model/User.java
@@ -1,4 +1,18 @@
 package ru.whattime.whattime.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
 }
