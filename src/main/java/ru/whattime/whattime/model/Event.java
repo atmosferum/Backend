6f0c211cf309703982d5652cbf9a66c9c2bd4cbb
kostoev05 <kostoev.05@mail.ru>
2022-06-17 22:06:43 +0300

    realize event model

--- src/main/java/ru/whattime/whattime/model/Event.java
+++ src/main/java/ru/whattime/whattime/model/Event.java
@@ -1,4 +1,34 @@
 package ru.whattime.whattime.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "events")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Event {
+
+    @Id
+    @Column(nullable = false)
+    private String uuid;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    private User owner;
+
+    @Column(nullable = false)
+    private Long createdAt;
+
 }
