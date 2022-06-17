89832d38f3019490125036b5a9e6b9f307c42c52
kostoev05 <kostoev.05@mail.ru>
2022-06-17 22:07:16 +0300

    realize interval model

--- src/main/java/ru/whattime/whattime/model/Interval.java
+++ src/main/java/ru/whattime/whattime/model/Interval.java
@@ -1,4 +1,27 @@
 package ru.whattime.whattime.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Interval {
+    @Id
+    @Column(unique = true, nullable = false)
+    private String uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User owner;
+
+    @Column(nullable = false)
+    private long startTimeInEpochSeconds;
+
+    @Column(nullable = false)
+    private long endTimeInEpochSeconds;
 }
