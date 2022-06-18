fc77ba90b6aed6ce1d73fccd9a46d37c45377712
kostoev05 <kostoev.05@mail.ru>
2022-06-18 11:05:30 +0300

    updated AuthenticationFilter

--- src/main/java/ru/whattime/whattime/auth/AuthenticationFilter.java
+++ src/main/java/ru/whattime/whattime/auth/AuthenticationFilter.java
@@ -1,68 +1,50 @@
-package ru.whattime.whattime.filter;
+package ru.whattime.whattime.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.whattime.whattime.encoder.Base64Encoder;
 import ru.whattime.whattime.model.User;
 import ru.whattime.whattime.service.UserService;
 
 
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.HashSet;
-import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
-public class IdentificationFilter implements Filter {
-
-    @Value("${application.auth.cookie.name}")
-    private String cookieName;
+public class AuthenticationFilter implements Filter {
 
     private final UserService service;
+
+    private final AuthTokenProvider tokenProvider;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         if (request.getMethod().equals("POST")) {
-            String token = extractUserTokenFromCookies(request);
+            String token = tokenProvider.provideToken(request);
 
             if (token == null) {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             } else {
                 User user = parseToken(token);
 
-                if (service.getUserById(user.getId()).isEmpty()) {
+                if (service.findById(user.getId()).isEmpty()) {
                     response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 }
             }
         }
 
         filterChain.doFilter(request, response);
-    }
-
-    private String extractUserTokenFromCookies(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-
-        if (cookies == null) {
-            return null;
-        }
-
-        for (Cookie cookie : cookies) {
-            if (cookieName.equals(cookie.getName()) && !cookie.getValue().isBlank()) {
-                return cookie.getValue();
-            }
-        }
-        return null;
     }
 
     private User parseToken(String token) throws IOException {
