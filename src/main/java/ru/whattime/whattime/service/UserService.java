7f994b48cdb221f74cb0fa94f969b1c6bc6ad1d3
kostoev05 <kostoev.05@mail.ru>
2022-06-18 09:01:47 +0300

    updated UserService.java

--- src/main/java/ru/whattime/whattime/service/UserService.java
+++ src/main/java/ru/whattime/whattime/service/UserService.java
@@ -1,4 +1,24 @@
 package ru.whattime.whattime.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.whattime.whattime.model.User;
+import ru.whattime.whattime.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository repository;
+
+    public User addUser(User user) {
+        return repository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return repository.findById(id);
+    }
+
+
 }
