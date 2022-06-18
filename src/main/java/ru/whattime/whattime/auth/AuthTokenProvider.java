f6e94b44b1645a7cbdef833845207f2cf29874f7
kostoev05 <kostoev.05@mail.ru>
2022-06-18 16:27:08 +0300

    updated AuthTokenProvider

--- src/main/java/ru/whattime/whattime/auth/AuthTokenProvider.java
+++ src/main/java/ru/whattime/whattime/auth/AuthTokenProvider.java
@@ -1,29 +1,36 @@
 package ru.whattime.whattime.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.whattime.whattime.encoder.Base64Encoder;
 import ru.whattime.whattime.model.User;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Base64;
 
 @Component
+@RequiredArgsConstructor
 public class AuthTokenProvider {
-    @Value("${application.auth.cookie.name}")
-    private String cookieName;
 
-    public String provideToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
+    private final Base64Encoder encoder;
 
-        if (cookies == null) {
-            return null;
-        }
-
-        for (Cookie cookie : cookies) {
-            if (cookieName.equals(cookie.getName()) && !cookie.getValue().isBlank()) {
-                return cookie.getValue();
-            }
-        }
-        return null;
+    public String provideToken(User user) {
+        return encoder.encodeUser(user);
     }
+
+    public User parseToken(String token) throws IOException {
+        String[] parts = token.split("\\.");
+        String body = parts[1];
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(decoder.decode(body), User.class);
+    }
+
+
 }
