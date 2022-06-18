96cac011cb145ba7223bb6cbb848c6af2e0ce83c
kostoev05 <kostoev.05@mail.ru>
2022-06-18 11:03:45 +0300

    added UserMapper

--- src/main/java/ru/whattime/whattime/mapper/UserMapper.java
+++ src/main/java/ru/whattime/whattime/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package ru.whattime.whattime.mapper;
 
+import org.mapstruct.Mapper;
+import ru.whattime.whattime.dto.UserDTO;
+import ru.whattime.whattime.model.User;
+
+import java.util.List;
+
+@Mapper
 public interface UserMapper {
+    UserDTO toUserDTO(User user);
+    List<UserDTO> toUserDTOs(List<User> users);
+    User toUser(UserDTO userDTO);
 }
