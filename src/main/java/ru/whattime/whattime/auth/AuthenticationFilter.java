92cbe756989220754a17b874ef00f37c436830c8
kostoev05 <kostoev.05@mail.ru>
2022-06-18 17:16:21 +0300

    updated AuthenticationFilter

--- src/main/java/ru/whattime/whattime/auth/AuthenticationFilter.java
+++ src/main/java/ru/whattime/whattime/auth/AuthenticationFilter.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Base64;
 
+@Component
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
 
