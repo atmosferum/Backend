b987580b43616b6c6be7bd376a598c576e729da5
kostoev05 <kostoev.05@mail.ru>
2022-06-18 09:01:34 +0300

    updated UserRepository.java

--- src/main/java/ru/whattime/whattime/repository/UserRepository.java
+++ src/main/java/ru/whattime/whattime/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package ru.whattime.whattime.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.whattime.whattime.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
