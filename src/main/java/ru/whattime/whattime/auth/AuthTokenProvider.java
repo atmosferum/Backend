3dfb6034b1ba0413fdec1f8d754300bfac4963ff
kostoev05 <kostoev.05@mail.ru>
2022-06-19 14:26:16 +0300

    updated AuthTokenProvider

--- src/main/java/ru/whattime/whattime/auth/AuthTokenProvider.java
+++ src/main/java/ru/whattime/whattime/auth/AuthTokenProvider.java
@@ -16,14 +16,14 @@ public class AuthTokenProvider {
 
     private final Base64Encoder encoder;
 
+    private final ObjectMapper mapper;
+
     public String provideToken(User user) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
         return encoder.encode(mapper.writeValueAsString(user));
     }
 
     public User parseToken(String token) throws IOException {
         Base64.Decoder decoder = Base64.getUrlDecoder();
-        ObjectMapper mapper = new ObjectMapper();
 
         return mapper.readValue(decoder.decode(token), User.class);
     }
