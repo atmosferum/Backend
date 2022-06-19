64280d7714ae9d070765c0af25188bd2ecb9119d
kostoev05 <kostoev.05@mail.ru>
2022-06-19 22:05:03 +0300

    added AuthController Test

---
+++ src/test/java/ru/whattime/whattime/controller/AuthControllerTest.java
@@ -0,0 +1,46 @@
+package ru.whattime.whattime.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.whattime.whattime.model.User;
+import ru.whattime.whattime.repository.UserRepository;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    void loginTest() throws Exception {
+        User user = new User(1L, "John");
+
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/v1/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(user));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+}
