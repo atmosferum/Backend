d6912e662ffec8cee404b6ec18a5a414cc7e0cca
kostoev05 <kostoev.05@mail.ru>
2022-06-19 14:39:25 +0300

    updated Interval

--- src/main/java/ru/whattime/whattime/model/Interval.java
+++ src/main/java/ru/whattime/whattime/model/Interval.java
@@ -6,12 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
-
+@Entity
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
