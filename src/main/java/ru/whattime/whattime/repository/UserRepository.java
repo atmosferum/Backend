7e966b2c6976ffbaddc3f347ed67ff2fd1486fbe
Valentin Chizhov <valentin.chizhov@gmail.com>
2022-06-20 01:25:49 +0600

    Удалил @Repository у UserRepository, так как Spring сам понимает, что это Repository Bean исходя из того, что он наследует от JpaRepository.

--- src/main/java/ru/whattime/whattime/repository/UserRepository.java
+++ src/main/java/ru/whattime/whattime/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package ru.whattime.whattime.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.whattime.whattime.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
