96b318793a5c80c9f9119cea8143a0ac576ed551
kostoev05 <kostoev.05@mail.ru>
2022-06-20 19:41:34 +0300

    updated EventController

--- src/main/java/ru/whattime/whattime/controller/EventController.java
+++ src/main/java/ru/whattime/whattime/controller/EventController.java
@@ -2,17 +2,16 @@ package ru.whattime.whattime.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.whattime.whattime.dto.EventDTO;
 import ru.whattime.whattime.model.Event;
 import ru.whattime.whattime.service.EventService;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/event")
@@ -21,11 +20,14 @@ public class EventController {
 
     private final EventService service;
 
-    @PostMapping
-    public ResponseEntity<?> createEvent(@Validated @RequestBody EventDTO eventDto) throws URISyntaxException {
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> createEvent(@Valid @RequestBody EventDTO eventDto) {
         Event event = service.createEvent(eventDto);
 
-        return ResponseEntity.created(new URI("http://localhost:8080/event/" + event.getUuid())).build();
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(event.getUuid())
+                .toUri()).build();
     }
 
 
