a63023b1bd279fdbd60239e466aa86618e8f27c1
kostoev05 <kostoev.05@mail.ru>
2022-06-20 11:22:07 +0300

    updated create event post

--- src/main/java/ru/whattime/whattime/controller/EventController.java
+++ src/main/java/ru/whattime/whattime/controller/EventController.java
@@ -1,4 +1,36 @@
 package ru.whattime.whattime.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.whattime.whattime.auth.AuthTokenProvider;
+import ru.whattime.whattime.dto.EventDTO;
+import ru.whattime.whattime.model.Event;
+import ru.whattime.whattime.service.EventService;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("api/v1/event")
+@RequiredArgsConstructor
 public class EventController {
+
+    private final EventService service;
+
+    private final AuthTokenProvider tokenProvider;
+
+
+    @PostMapping
+    public ResponseEntity<?> createEvent(@Validated @RequestBody EventDTO eventDto) throws URISyntaxException {
+        Event event = service.createEvent(eventDto);
+
+        return ResponseEntity.created(new URI("http://localhost:8080/event/" + event.getUuid())).build();
+    }
+
+
 }
--- src/main/java/ru/whattime/whattime/service/EventService.java
+++ src/main/java/ru/whattime/whattime/service/EventService.java
@@ -1,4 +1,29 @@
 package ru.whattime.whattime.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.whattime.whattime.dto.EventDTO;
+import ru.whattime.whattime.mapper.EventMapper;
+import ru.whattime.whattime.model.Event;
+import ru.whattime.whattime.repository.EventRepository;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class EventService {
+
+    private final EventRepository repository;
+
+    private final EventMapper mapper;
+
+    private final UserService userService;
+
+    public Event createEvent(EventDTO eventDto) {
+        eventDto.setUuid(UUID.randomUUID().toString());
+        eventDto.setOwner(userService.getCurrentUser());
+
+
+        return repository.save(mapper.toEntity(eventDto));
+    }
 }
