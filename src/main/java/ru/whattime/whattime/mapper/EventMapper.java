b544f70fd5a2adbbb2754daa385c63e6b3d8bbf9
kostoev05 <kostoev.05@mail.ru>
2022-06-20 09:57:07 +0300

    updated EventMapper

--- src/main/java/ru/whattime/whattime/mapper/EventMapper.java
+++ src/main/java/ru/whattime/whattime/mapper/EventMapper.java
@@ -1,4 +1,23 @@
 package ru.whattime.whattime.mapper;
 
+import org.mapstruct.Mapper;
+import ru.whattime.whattime.dto.EventDTO;
+import ru.whattime.whattime.model.Event;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Mapper
 public interface EventMapper {
+    EventDTO toDTO(Event event);
+    Event toEntity(EventDTO eventDto);
+
+    default Long map(LocalDateTime value) {
+        return value.atZone(ZoneId.systemDefault()).toEpochSecond();
+    }
+
+    default LocalDateTime map(Long value) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(value), ZoneId.systemDefault());
+    }
 }
