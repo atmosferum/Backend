28d219a9d6501074a4a1c8592cc7e1ce3aa11304
kostoev05 <kostoev.05@mail.ru>
2022-06-20 09:56:53 +0300

    updated EventDTO

--- src/main/java/ru/whattime/whattime/dto/EventDTO.java
+++ src/main/java/ru/whattime/whattime/dto/EventDTO.java
@@ -1,4 +1,12 @@
 package ru.whattime.whattime.dto;
 
+import lombok.Data;
+
+@Data
 public class EventDTO {
+    private String uuid;
+    private String title;
+    private String description;
+    private UserDTO owner;
+    private Long createdAt;
 }
