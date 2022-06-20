4741e8ad9c9ba829a720912d075942a10531333d
kostoev05 <kostoev.05@mail.ru>
2022-06-20 19:45:46 +0300

    updated User

--- src/main/java/ru/whattime/whattime/model/User.java
+++ src/main/java/ru/whattime/whattime/model/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="users")
@@ -20,4 +21,7 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Event> event;
 }
