27174c940bbf116ddbac14a5b5917eff711da441
kostoev05 <kostoev.05@mail.ru>
2022-06-21 21:10:34 +0300

    Commit

--- src/main/java/ru/whattime/whattime/controller/EventController.java
+++ src/main/java/ru/whattime/whattime/controller/EventController.java
@@ -3,6 +3,7 @@ package ru.whattime.whattime.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -10,12 +11,16 @@ import ru.whattime.whattime.dto.EventDto;
 import ru.whattime.whattime.dto.IntervalDto;
 import ru.whattime.whattime.service.EventService;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/events")
+@Validated
 @RequiredArgsConstructor
 public class EventController {
 
@@ -31,6 +36,20 @@ public class EventController {
                 .toUri();
 
         return ResponseEntity.created(uri).build();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleException(ConstraintViolationException constraintViolationException) {
+        Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
+        String errorMessage = "";
+        if (!violations.isEmpty()) {
+            StringBuilder builder = new StringBuilder();
+            violations.forEach(violation -> builder.append(" " + violation.getMessage()));
+            errorMessage = builder.toString();
+        } else {
+            errorMessage = "ConstraintViolationException occured.";
+        }
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping(path = "/{id}/intervals", consumes = "application/json", produces = "application/json")
