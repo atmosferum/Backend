ccbad0f7535e41bb257e988cd8af524735ad9155
kostoev05 <kostoev.05@mail.ru>
2022-06-21 09:12:29 +0300

    updated EventService

--- src/main/java/ru/whattime/whattime/service/EventService.java
+++ src/main/java/ru/whattime/whattime/service/EventService.java
@@ -4,9 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.whattime.whattime.dto.EventDTO;
 import ru.whattime.whattime.mapper.EventMapper;
-import ru.whattime.whattime.mapper.UserMapper;
 import ru.whattime.whattime.model.Event;
+import ru.whattime.whattime.model.User;
 import ru.whattime.whattime.repository.EventRepository;
+import ru.whattime.whattime.repository.UserRepository;
 
 import java.util.UUID;
 
@@ -16,16 +17,21 @@ public class EventService {
 
     private final EventRepository repository;
 
-    private final EventMapper eventMapper;
+    private final UserRepository userRepository;
 
-    private final UserMapper userMapper;
+    private final EventMapper eventMapper;
 
     private final UserService userService;
 
     public Event createEvent(EventDTO eventDto) {
         Event event = eventMapper.toEntity(eventDto);
         event.setUuid(UUID.randomUUID());
-        event.setOwner(userMapper.toEntity(userService.getCurrentUser()));
+
+        User user = userRepository.getReferenceById(userService.getCurrentUser().getId());
+
+        event.setOwner(user);
+
+        user.getEvents().add(event);
 
         return repository.save(event);
     }
