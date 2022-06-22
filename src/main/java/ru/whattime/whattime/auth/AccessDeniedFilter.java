26154ef8a13a446d3fb34f20c72c69c4b123d639
Valentin Chizhov <valentin.chizhov@gmail.com>
2022-06-22 19:00:12 +0600

    Optimize import in AccessDeniedFilter

--- src/main/java/ru/whattime/whattime/auth/AccessDeniedFilter.java
+++ src/main/java/ru/whattime/whattime/auth/AccessDeniedFilter.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 @Component
 @Order(2)
